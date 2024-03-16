c07f9c1eb2319e2819bbcedd5ad670b3c28de32c
Ludmila <milaabl2405@gmail.com>
2024-03-16 18:20:44 +0300

    feat: buy USDT from WETH after depositing ETH to WETH on Ganache function (needed for my testing purposes)

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -1,10 +1,12 @@
 import { createConfig, http } from "wagmi";
-import { goerli, mainnet } from "viem/chains";
+import { Chain, goerli, mainnet } from "viem/chains";
 import { injected, walletConnect } from '@wagmi/connectors';
 
 export const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
 
-export const customChains = [mainnet, goerli] as const;
+const ganacheEthereumFork = { ...mainnet, rpcUrls: { default: { http: [`http://127.0.0.1:8545` ] } } } satisfies Chain;
+
+export const customChains = [ganacheEthereumFork] as const;
 
 export const wagmiClient = createConfig({
   chains: customChains,
